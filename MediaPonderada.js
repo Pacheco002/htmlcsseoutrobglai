f375f553264e336d0394a63be0acbdac63bf9e0b
Pacheco002 <125073097+Pacheco002@users.noreply.github.com>
2023-07-28 16:41:51 -0300

    Add files via upload

---
+++ MediaPonderada.js
@@ -0,0 +1,12 @@
+function mediaponderada(){Array.
+    var total = 0;
+    for (var i = 0; i <)
+    total += media [i] *
+var avg = total /
+return avg:
+
+const numbers = [1, 2, 3, 4, 5];
+const weights = [1, 2, 3, 4, 5];
+const avg =
+console.log(avg); //output:
+}
